0b5f9d5317a04fc8089fa42b5cf2b7c902301963
ray <656482519@qq.com>
2023-10-27 11:12:40 +0800

    修正1

--- src/main/java/top/wml/ApiDemoApplication.java
+++ src/main/java/top/wml/ApiDemoApplication.java
@@ -2,6 +2,7 @@ package top.wml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class ApiDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiDemoApplication.class, args);
